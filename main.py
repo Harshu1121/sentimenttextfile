c1d8ebed5bbc148658a2d09dd4b57c63c8d1b85b
Harshvardhan <harshvardhanpasalkar@gmail.com>
2024-03-08 12:44:07 +0530

    text

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+import cleantext
+st.header('Sentiment Analysis')
+
+with st.expander('Analyze Text'):
+    text = st.text_input('Text here: ')
+    if text:
+        blob = TextBlob(text)
+        st.write('Polarity: ', round(blob.sentiment.polarity, 2))
+        st.write('Subjectivity: ', round(blob.sentiment.subjectivity, 2))
+
+    pre = st.text_input('Clean Text: ')
+    if pre:
+        st.write(cleantext.clean(pre, clean_all=False, extra_spaces=True,
+                                 stopwords=True, lowercase=True, numbers=True, punct=True))
+
+with st.expander('Analyze CSV/TXT'):
+    upl = st.file_uploader('Upload file', type=['csv', 'txt'])  # Allow CSV and TXT file uploads
+
+    def analyze_sentiment(text):
+        blob = TextBlob(text)
+        return round(blob.sentiment.polarity, 2), round(blob.sentiment.subjectivity, 2)
